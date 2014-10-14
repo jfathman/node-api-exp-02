f0d8e0f8b7bca63cab28edee751336374f959d67
Jim Fathman <jfathman@esi-estech.com>
2014-10-14 22:07:58 +0000

    Add Jenkins build step logging.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -6,17 +6,23 @@ set -e
 
 # Assumes prior export ARTIFACTORY_ACCOUNT is available.
 
-# Get application name and version from package.json.
+echo
+echo "Get application name and version from package.json."
+echo
 
 APP_NAME=$(cat package.json | jq -r '.name')
 
 APP_VERSION=$(cat package.json | jq -r '.version')
 
-# Build Docker image.
+echo
+echo "Build Docker image."
+echo
 
 docker build -t ${APP_NAME}:${APP_VERSION} .
 
-# Remove untagged images after Docker reuses repo:tag for new build.
+echo
+echo "Remove untagged images after Docker reuses repo:tag for new build."
+echo
 
 UNTAGGED=$(docker images --filter "dangling=true" -q)
 
@@ -24,17 +30,23 @@ if [ ! -z "$UNTAGGED" ]; then
   docker rmi ${UNTAGGED};
 fi
 
-# Run mock tests including load test in Docker container.
+echo
+echo "Run mock tests including load test in Docker container."
+echo
 
 docker run --rm ${APP_NAME}:${APP_VERSION} grunt --no-color test
 
-# Retrieve build artifacts from Docker container.
+echo
+echo "Retrieve build artifacts from Docker container."
+echo
 
 mkdir -p artifacts
 
 docker run --rm -v ${PWD}/artifacts:/mnt ${APP_NAME}:${APP_VERSION} /bin/bash -c 'cp artifacts/* /mnt/.'
 
-# Tag Docker image for Artifactory, push image to Artifactory, and remove tag.
+echo
+echo "Tag Docker image for Artifactory, push image to Artifactory, and remove tag."
+echo
 
 docker tag ${APP_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
 
@@ -42,3 +54,6 @@ docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSI
 
 docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
 
+echo
+echo "Build complete."
+echo
