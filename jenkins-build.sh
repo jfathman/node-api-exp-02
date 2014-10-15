b99d959ec01e00862630a062f8e48961c643468b
Jim Fathman <jfathman@esi-estech.com>
2014-10-15 19:38:08 +0000

    Add runtime annotation.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -6,97 +6,76 @@ set -e
 
 # Assumes prior export ARTIFACTORY_ACCOUNT is available.
 
-set +x
-echo
-echo "---------------------------------------------------"
-echo "Get application name and version from package.json."
-echo "---------------------------------------------------"
-echo
-set -x
+main()
+{
+  set +x
+  annotate "Get application name and version from package.json."
 
-APP_NAME=$(cat package.json | jq -r '.name')
+  APP_NAME=$(cat package.json | jq -r '.name')
 
-APP_VERSION=$(cat package.json | jq -r '.version')
+  APP_VERSION=$(cat package.json | jq -r '.version')
 
-set +x
-echo
-echo "-------------------"
-echo "Build Docker image."
-echo "-------------------"
-echo
-set -x
+  set +x
+  annotate "Build Docker image."
 
-docker build -t ${APP_NAME}:${APP_VERSION} .
+  docker build -t ${APP_NAME}:${APP_VERSION} .
 
-set +x
-echo
-echo "------------------------------------------------------------------"
-echo "Remove untagged images after Docker reuses repo:tag for new build."
-echo "------------------------------------------------------------------"
-echo
-set -x
+  set +x
+  annotate "Remove untagged images after Docker reuses repo:tag for new build."
 
-UNTAGGED=$(docker images --filter "dangling=true" -q)
+  UNTAGGED=$(docker images --filter "dangling=true" -q)
 
-if [ ! -z "$UNTAGGED" ]; then
-  docker rmi ${UNTAGGED};
-fi
+  if [ ! -z "$UNTAGGED" ]; then
+    docker rmi ${UNTAGGED};
+  fi
 
-set +x
-echo
-echo "-------------------------------------------------------"
-echo "Run mock tests including load test in Docker container."
-echo "-------------------------------------------------------"
-echo
-set -x
+  set +x
+  annotate "Run mock tests including load test in Docker container."
 
-docker run --rm ${APP_NAME}:${APP_VERSION} grunt --no-color test
+  docker run --rm ${APP_NAME}:${APP_VERSION} grunt --no-color test
 
-set +x
-echo
-echo "-----------------------------------------------"
-echo "Retrieve build artifacts from Docker container."
-echo "-----------------------------------------------"
-echo
-set -x
+  set +x
+  annotate "Retrieve build artifacts from Docker container."
 
-mkdir -p artifacts
+  mkdir -p artifacts
 
-docker run --rm -v ${PWD}/artifacts:/mnt ${APP_NAME}:${APP_VERSION} /bin/bash -c 'cp artifacts/* /mnt/.'
+  docker run --rm -v ${PWD}/artifacts:/mnt ${APP_NAME}:${APP_VERSION} /bin/bash -c 'cp artifacts/* /mnt/.'
 
-set +x
-echo
-echo "---------------------------------"
-echo "Tag Docker image for Artifactory."
-echo "---------------------------------"
-echo
-set -x
+  set +x
+  annotate "Tag Docker image for Artifactory."
 
-docker tag ${APP_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker tag ${APP_NAME}:${APP_VERSION} ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
 
-set +x
-echo
-echo "---------------------------------"
-echo "Push Docker image to Artifactory."
-echo "---------------------------------"
-echo
-set -x
+  set +x
+  annotate "Push Docker image to Artifactory."
 
-docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker push ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
 
-set +x
-echo
-echo "---------------------------------"
-echo "Remove tag added for Artifactory."
-echo "---------------------------------"
-echo
-set -x
+  set +x
+  annotate "Remove tag added for Artifactory."
 
-docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
+  docker rmi ${ARTIFACTORY_ACCOUNT}.artifactoryonline.com/${APP_NAME}:${APP_VERSION}
 
-set +x
-echo
-echo "---------------"
-echo "Build complete."
-echo "---------------"
-echo
+  set +x
+  annotate "Build complete."
+}
+
+annotate()
+{
+  desc=$1
+  date=$(date)
+  desc_len=${#desc}
+  date_len=${#date}
+  max_len=$(($desc_len > $date_len ? $desc_len : $date_len))
+  dashes=$(eval printf -- '-%.s' {1..$max_len}; echo)
+  echo
+  echo "$dashes"
+  echo "$desc"
+  echo "$date"
+  echo "$dashes"
+  echo
+  set -x
+}
+
+main "$@"
+
